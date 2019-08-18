4a5960e93db956f326b7264c27025f847a17a4ea
Nicola Rankin <nicola.rankin@gmail.com>
2019-08-18 20:10:45 +1200

    rename to more generic

--- test/array_flattener_test.rb
+++ test/array_flattener_test.rb
@@ -1,15 +1,15 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require_relative '../lib/integer_array_flattener'
+require_relative '../lib/array_flattener'
 
-describe IntegerArrayFlattener do
+describe ArrayFlattener do
 	it 'returns an array' do
-		IntegerArrayFlattener.flatten([1]).must_be_kind_of(Array)
+		ArrayFlattener.flatten([1]).must_be_kind_of(Array)
 	end
 
 	it 'only accepts arrays' do
-		lambda { IntegerArrayFlattener.flatten("john") }.must_raise(ArgumentError)
-		lambda { IntegerArrayFlattener.flatten(5) }.must_raise(ArgumentError)
+		lambda { ArrayFlattener.flatten("john") }.must_raise(ArgumentError)
+		lambda { ArrayFlattener.flatten(5) }.must_raise(ArgumentError)
 	end
 
 	it 'flattens an array of arbitrarily nested arrays of integers' do
@@ -22,7 +22,7 @@ describe IntegerArrayFlattener do
 
 	def check_this(array)
 		expected_result = array.flatten
-		result = IntegerArrayFlattener.flatten(array)
+		result = ArrayFlattener.flatten(array)
 		assert_equal(expected_result, result)
 	end	
 
